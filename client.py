071b489dd00205a02d1b699e9187006cc3c4263b
rafaelmnf <rafaelnfilho@gmail.com>
2026-08-21 13:19:05 -0300

    Cliente e server funcionando. Falta fazer a loteria

--- client.py
+++ client.py
@@ -1,19 +1,70 @@
 import socket
 import threading
+import sys
+from datetime import datetime
 
 HOST = "127.0.0.1"
 PORT = 5000
 
 # Conecta o cliente no socket
+def conectarCliente():
+    try: 
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST, PORT))
+        return client
+    except ConnectionRefusedError:
+        print("Servidor não encontrado")
+        sys.exit(1)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
-    socket.connect((HOST, PORT))
-    data = socket.recv(1024).decode('utf-8') # Recebe os dados já legíveis
-    print(f"{data}") # Deve printar os dados enviados do server 
+
+# Cria thread que lê input do usuário e envia para o server em loop infinito
+def enviarServer(client):
+    while True:
+        try:
+            mensagem = input("Digite para configurar (:) ou somente numeros para apostar: ")
+            if mensagem.strip(): # se tiver mensagem
+                # Envia a mensagem codificada em bytes
+                client.send(mensagem.encode('utf-8'))
+        except (EOFError, KeyboardInterrupt):
+            print("\nEncerrando cliente...")
+            client.close()
+            break
+
+    
+
+# Cria thread que lê do socket e imprime na tela
+def receberServer(client):
+    while True:
+        data = client.recv(1024).decode('utf-8')
+        if not data:
+            print("Conexão encerrada pelo servidor")
+            client.close()    
+            break
+        print(data)
+            
 
 
-thread = threading.Thread()
-# Inicia a thread
-thread.start()
-# Espera pela thread terminar
-thread.join()
+def main():
+    client = conectarCliente() # se a conexão for bem sucedida, imprimir o horário dela
+
+    horario = datetime.now().strftime("%H:%M:%S")   
+    print("------------------------------------------------------------")
+    print(f"{horario}: Conectado!\n Você pode:")
+    print(" • Configurar: :inicio <num> | :fim <num> | :qtd <num>")
+    print(" • Apostar: Digite os números separados por espaço (ex: 5 12 30)")
+    print("------------------------------------------------------------")
+
+    # Ao passar os args da função ele espera uma tupla, logo, precisa colocar em (x,y)
+    t1 = threading.Thread(target=enviarServer, args=(client,), daemon=True)
+    # Inicia a thread
+    t1.start()
+
+    t2 = threading.Thread(target=receberServer, args=(client,), daemon=False)
+    # Inicia a thread
+    t2.start()
+    t2.join()
+
+
+# se tiver neste arquivo rodando executa a main. Evita que ao importar ele rode automaticamente
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,25 +1,76 @@
 import socket
 import threading
-from datetime import datetime
+import random
+import time
 
 HOST = "127.0.0.1" # Padrão localhost
 PORT = 5000 # Portas a cima de 1023 são sem privilégio, ou seja, podem ser acessadas sem precisar pedir
 
+# Configurando a loteria
+loteria = []
+sorteados = []
+
+# Adiciona 5 numeros aleatórios na lista de numeros sorteados
+for i in range(5):
+    sorteados.append(random.randint(0, 100))
+
+
+# Cria thread que recebe dados do cliente (Lê do socket e usa como input para processamento) --> loop infinito de recebimento
+def lerCliente(connection, address):
+    # recebe dados enviados do cliente -> recv = receive é bloqueante e espera que aja alguma mensagem
+    while True:
+        data = connection.recv(1024).decode('utf-8')
+        if not data:
+            print(f" Cliente {address} desconectou.")
+            break
+
+        print(f"{data}")
+
+    connection.close()
+
+
+# Cria thread que auxilia no envio de dados (envia o output do processamento do server para o cliente)
+def enviarCliente(connection, address):
+
+    while True:
+        time.sleep(10) # depois troca para 60, 10 para testes
+        try:
+            connection.send(sorteados.encode('utf-8'))
+        except ConnectionResetError:
+            break
+        
+
 # socket.AF_INET = para definir que vamos usar IPv4 e socket.SOCK_STREAM para dizer que é TCP
 # Com o with vc não precisa se preocupar em fechar o socket.close(), ele já gerencia no contexto.
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
-    socket.bind((HOST, PORT))
+# Cria socket servidor
+def iniciarServer():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
     # servidor está esperando o handshake
-    socket.listen()
-    # connection recebe o socket atrelado ao cliente conectado. Já o endereço é uma tupla contendo o ip e a porta do cliente
-    connection, address = socket.accept() # aqui ele bloqueia a execução do código e espera um cliente
-    # recebe dados enviados do cliente -> recv = receive
-    data = socket.recv(1024).decode('utf-8')
-
-horario = datetime.now()
-# Manda para o cliente conectado. encode é usado para enviar por bytes 
-connection.send(f"{horario}: CONECTADO!!".encode('utf-8'))
+    server.listen()
+    # connection recebe o socket atrelado ao cliente conectado. Já o endereço é uma tupla contendo o ip e a porta do client
+    return server
 
 
+def main():
+    server = iniciarServer()
+    try:
+        # Fica esperando em um loop infinito os clientes
+        while True:
+            connection, address = server.accept() # aqui ele bloqueia a execução do código e espera um cliente
+            # Quando se conectar, cria as threads de auxílio para ele
+            # Daemon true significa que quando o programa termina, a thread termina junto com ele. Logo, não precisamos esperar a thread com join, visto que o programa estará em um eterno loop de espera de clientes
+            t1 = threading.Thread(target=lerCliente, args=(connection, address), daemon=True)
+            t1.start()
 
+            t2 = threading.Thread(target=enviarCliente, args=(connection, address), daemon=True)
+            t2.start()
+    # Essa exeção acontece quando o usuário encerra o programa 
+    except KeyboardInterrupt:
+        print("\nServidor finalizado pelo teclado (Ctrl+C).")
+    finally:
+        server.close()
+
+
+if __name__ == '__main__':
+    main()
